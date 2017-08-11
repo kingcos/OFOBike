af3dd5af03154ee6cd3d4d7278e6b9de9dfa08cb
kingcos <2821836721v@gmail.com>
2017-08-12 02:29:18 +0800

    Search bikes nearby & show annotations

--- OFOBike/Controller/HomeViewController.swift
+++ OFOBike/Controller/HomeViewController.swift
@@ -15,6 +15,9 @@ class HomeViewController: UIViewController {
     
     // Map related
     var mapView: MAMapView!
+    var searchAPI: AMapSearchAPI!
+    
+    var isSearchNearby = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,11 +31,13 @@ class HomeViewController: UIViewController {
 extension HomeViewController {
     fileprivate func setup() {
         setupNavigationItems()
-        
         setupMapView()
         setupPanelView()
+        
+        setupSearchAPI()
     }
     
+    // MARK: View
     private func setupNavigationItems() {
         navigationItem.titleView = UIImageView(image: UIImage(named: "ofoLogo"))
         
@@ -57,10 +62,6 @@ extension HomeViewController {
             view.addGestureRecognizer(revealVC.panGestureRecognizer())
         }
     }
-    
-    private func setupPanelView() {
-        view.bringSubview(toFront: panelView)
-    }
 
     private func setupMapView() {
         mapView = MAMapView(frame: view.bounds)
@@ -71,16 +72,78 @@ extension HomeViewController {
         
         view.addSubview(mapView)
     }
+    
+    private func setupPanelView() {
+        view.bringSubview(toFront: panelView)
+    }
+    
+    // MARK: Logic
+    private func setupSearchAPI() {
+        searchAPI = AMapSearchAPI()
+        searchAPI.delegate = self
+    }
 }
 
 // MARK: Button actions
 extension HomeViewController {
     @IBAction func clickLocateButton(_ sender: UIButton) {
-        
+        searchBikesNearby()
     }
 }
 
 // MARK: MAMapViewDelegate
 extension HomeViewController: MAMapViewDelegate {
     
+}
+
+// MARK: AMapSearchDelegate
+extension HomeViewController: AMapSearchDelegate {
+    // POI 搜索完成后回调
+    func onPOISearchDone(_ request: AMapPOISearchBaseRequest!, response: AMapPOISearchResponse!) {
+        // 保证周边有车
+        guard response.count > 0 else { return }
+        
+        var annotations = [MAPointAnnotation]()
+        annotations = response.pois.map {
+            let annotation = MAPointAnnotation()
+            annotation.coordinate = CLLocationCoordinate2D(latitude: CLLocationDegrees($0.location.latitude),
+                                                           longitude: CLLocationDegrees($0.location.longitude))
+            // 根据距离区分红包车/普通车
+            if $0.distance < 200 {
+                annotation.title = "红包区域内开锁任意小黄车"
+                annotation.subtitle = "骑行 10 分钟可获得现金红包"
+            } else {
+                annotation.title = "正常可用"
+            }
+            return annotation
+        }
+        
+        mapView.addAnnotations(annotations)
+        
+        if isSearchNearby {
+            mapView.showAnnotations(annotations, animated: true)
+            isSearchNearby = !isSearchNearby
+        }
+    }
+}
+
+// MARK: POI search
+extension HomeViewController {
+    // 搜索附近的小黄车
+    func searchBikesNearby() {
+        searchCustomLocation(mapView.userLocation.coordinate)
+    }
+    
+    // 根据坐标（中心点）搜索
+    func searchCustomLocation(_ center: CLLocationCoordinate2D) {
+        // 附近 POI 搜索请求
+        let request = AMapPOIAroundSearchRequest()
+        request.location = AMapGeoPoint.location(withLatitude: CGFloat(center.latitude),
+                                                 longitude: CGFloat(center.longitude))
+        request.keywords = "餐馆"
+        request.radius = 500
+        request.requireExtension = true
+        
+        searchAPI.aMapPOIAroundSearch(request)
+    }
 }
