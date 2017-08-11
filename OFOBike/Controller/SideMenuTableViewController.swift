acb03523b19f51c3d77bbc9fe706b4fafb2ad3c0
kingcos <2821836721v@gmail.com>
2017-08-11 21:58:32 +0800

    Layout SideMenuTableViewController

---
+++ OFOBike/Controller/SideMenuTableViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SideMenuTableViewController.swift
+//  OFOBike
+//
+//  Created by kingcos on 05/05/2017.
+//  Copyright © 2017 kingcos. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuTableViewController: UITableViewController {
+
+    @IBOutlet weak var iconImageView: UIImageView!
+    @IBOutlet weak var userNameLabel: UILabel!
+    @IBOutlet weak var certImageView: UIImageView!
+    @IBOutlet weak var certLabel: UILabel!
+    @IBOutlet weak var creditLabel: UILabel!
+    @IBOutlet weak var balanceLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
